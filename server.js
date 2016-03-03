d68c81b7f29aeb51dab802a7724c2fdbe996945a
Tara Matthews <matthet@tcd.ie>
2016-03-03 16:22:30 +0000

    Weds work

--- server.js
+++ server.js
@@ -28,8 +28,10 @@ var client = new Twitter({
 });
 
 filepath = "Output.txt";
-divider = "--------------------------"
-fs.writeFile(filepath , 'Rumour Output \n' + divider + '\n', function(err) {
+tweet_divider = "--------------------------"
+sub_div = "~~~~~"
+
+fs.writeFile(filepath , 'Rumour Output \n' + tweet_divider + '\n', function(err) {
   if(err) {
     return console.log(err);
   }
@@ -59,9 +61,10 @@ app.get('/search', function(req, res) {
 
     for (i = 0; i < tweets.statuses.length; i++) {
       tweet_text = JSON.stringify(tweets.statuses[i].text);
-
-      tweet_number = match_count + 1;
-        tweet_string = tweet_number + '.\n' + divider + '\n' + parseTweet(tweets.statuses[i]);
+      
+      if ((tweet_text.search(/apple/i)) != -1 && (tweet_text.search(/march 18th/i)) != -1){
+        tweet_number = match_count + 1;
+        tweet_string = tweet_number + '.\n' + tweet_divider + '\n' + parseTweet(tweets.statuses[i]);
 
         fs.appendFile(filepath, tweet_string, function (err) {
           if(err) {
@@ -71,20 +74,7 @@ app.get('/search', function(req, res) {
         console.log("tweet added to Output.txt!");
         });
         match_count += 1;
-      
-      // if ((tweet_text.search(/apple/i)) != -1 && (tweet_text.search(/march 18th/i)) != -1){
-      //   tweet_number = match_count + 1;
-      //   tweet_string = tweet_number + '.\n' + divider + '\n' + parseTweet(tweets.statuses[i]);
-
-      //   fs.appendFile(filepath, tweet_string, function (err) {
-      //     if(err) {
-      //       return console.log(err);
-      //     }
-
-      //   console.log("tweet added to Output.txt!");
-      //   });
-      //   match_count += 1;
-      // }
+      }
     }
   });
 });
@@ -121,17 +111,23 @@ function parseTweet(tweet) {
   tweet_id = JSON.stringify(tweet.id);
   tweet_text = JSON.stringify(tweet.text);
 
-  // Impact features
-  tweet_location = tweet.place ? JSON.stringify(tweet.place.full_name) : tweet.place;
-  tweet_createdAt = JSON.stringify(tweet.created_at);
-  entities_hashtags = JSON.stringify(tweet.entities.hashtags);
-  entities_urls = JSON.stringify(tweet.entities.urls);
-  entities_user_mentions = JSON.stringify(tweet.entities.user_mentions);
-  entities_media = tweet.entities.media ? JSON.stringify(tweet.entities.media[0].media_url) : tweet.entities.media;
-  question_mark = tweet_text.indexOf('?') > -1 ? true : false;
-  smiling_emoticon = tweet_text.indexOf(':)') > -1 ? true : false;
+  // Impact / Engagement
   favorite_count = JSON.stringify(tweet.favorite_count);
   retweet_count = JSON.stringify(tweet.retweet_count);
+
+  // Tweet features for analysis
+  tweet_location = tweet.place ? JSON.stringify(tweet.place.full_name) : tweet.place;
+  tweet_createdAt = JSON.stringify(tweet.created_at);
+  entities_hashtags = JSON.stringify(tweet.entities.hashtags) == '[]' ? false : true;
+  entities_urls = JSON.stringify(tweet.entities.urls) == '[]' ? false : true;
+  entities_user_mentions = JSON.stringify(tweet.entities.user_mentions) == '[]' ? false : true;
+  entities_media = tweet.entities.media ? true : false;
+  word_retweet = tweet_text.search(/retweet/i) > -1 ? true : false;
+  abbrev_RT = tweet_text.search(/RT/) > -1 ? true : false;
+  question_mark = tweet_text.indexOf('?') > -1 ? true : false;
+  explanation_mark = tweet_text.indexOf('!') > -1 ? true : false;
+  quote = tweet.text.replace(/[^\""]/g, "").length == 2 ? true : false;
+  smiling_emoticon = tweet_text.indexOf(':)') > -1 ? true : false;
   default_profile = JSON.stringify(tweet.user.default_profile);
   default_profile_image = JSON.stringify(tweet.user.default_profile_image);
   user_createdAt = JSON.stringify(tweet.user.created_at);
@@ -142,17 +138,24 @@ function parseTweet(tweet) {
   account_verified = JSON.stringify(tweet.user.verified);
 
   tweet_string = 'ID: ' + tweet_id + '\n' + 
-                 'Text:' + tweet_text + '\n' + 
+                 'Text: ' + tweet_text + '\n' + 
+                 sub_div + '\n' +
+                 'Favourite Count: ' + favorite_count + '\n' + 
+                 'Retweet Count: ' + retweet_count + '\n' + 
+                 sub_div + '\n' +
                  'Tweet Location: ' + tweet_location + '\n' + 
-                 'Tweet Created At: ' + tweet_createdAt + '\n' + 
+                 'Tweet Time: ' + tweet_createdAt + '\n' + 
+                 'Tweet Length: ' + tweet.text.length + '\n' + 
                  'Hashtags: ' + entities_hashtags + '\n' + 
                  'URLs: ' + entities_urls + '\n' +
                  'User Mentions: ' + entities_user_mentions + '\n' + 
                  'Media: ' + entities_media + '\n' + 
+                 '\'Retweet\': ' + word_retweet + '\n' + 
+                 '\'RT\': ' + abbrev_RT + '\n' + 
                  'Question Mark: ' + question_mark + '\n' + 
+                 'Explanation Mark: ' + explanation_mark + '\n' + 
+                 'Quote: ' + quote + '\n' + 
                  'Smiling Emoticon: ' + smiling_emoticon + '\n' + 
-                 'Favorite Count: ' + favorite_count + '\n' + 
-                 'Retweet Count: ' + retweet_count + '\n' + 
                  'Default Profile: ' + default_profile + '\n' +
                  'Default Profile Image: ' + default_profile_image + '\n' + 
                  'User Created At: ' + user_createdAt + '\n' + 
@@ -161,7 +164,7 @@ function parseTweet(tweet) {
                  'Statuses Count: ' + statuses_count + '\n' + 
                  'Account Location: ' + account_location + '\n' + 
                  'Account Verified: ' + account_verified + '\n' + 
-                 divider + '\n';
+                 tweet_divider + '\n';
 
   return (tweet_string);
 }
