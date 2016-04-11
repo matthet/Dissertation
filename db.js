208e6161b37f533b85ac08ca67c8c4154703a613
Tara Matthews <matthet@tcd.ie>
2016-04-11 11:36:12 +0100

    Update mongolab connection URI

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
 module.exports = {
-	'url' : 'mongodb://tara:dbtara@ds011429.mlab.com:11429/rumours'
+	// 'url' : 'mongodb://tara:dbtara@ds011429.mlab.com:11429/rumours'
+	'url' : 'mongodb://heroku_lwvt23d6:sovd338kvg00ce2tqnn0v2ejnj@ds021650.mlab.com:21650/heroku_lwvt23d6'
+
 }
