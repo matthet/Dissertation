9161c9f03f63be4fe27c4906ff73b9e39b68b4ca
Tara Matthews <matthet@tcd.ie>
2016-04-11 11:00:29 +0100

    test for heroku 5

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ var gunz_vasectomy = require('./models/gunz_vasectomy');
 var calvert_leaving = require('./models/calvert_leaving');
 var giant_rat_london = require('./models/giant_rat_london');
 
-var Rumour = require('./models/trump_white_tshirts');
+var Rumour = require('./models/jenner_pregnant');
 
 var ss = require('simple-statistics');
 
@@ -100,126 +100,126 @@ app.get('/search', function(req, res) {
 
 // Scan database to find oldest tweet received.
 
-// function findLowestID(callback) {
-//   Rumour.find({}, 'id', function (err, tweet_ids) {
-//     lowest_id = '';
+function findLowestID(callback) {
+  Rumour.find({}, 'id', function (err, tweet_ids) {
+    lowest_id = '';
   
-//     if (tweet_ids.length > 0) {
-//       lowest_id = tweet_ids[0].id;
+    if (tweet_ids.length > 0) {
+      lowest_id = tweet_ids[0].id;
 
-//       for (i = 1; i < tweet_ids.length; i++) {
-//         if (tweet_ids[i].id.localeCompare(lowest_id) == -1){
-//           lowest_id = tweet_ids[i].id;
-//         }
-//       }
-//     }
-//     callback(lowest_id);
-//   });
-// }
+      for (i = 1; i < tweet_ids.length; i++) {
+        if (tweet_ids[i].id.localeCompare(lowest_id) == -1){
+          lowest_id = tweet_ids[i].id;
+        }
+      }
+    }
+    callback(lowest_id);
+  });
+}
 
 // Scan database to find newest tweet received.
 
-// function findGreatestID(callback) {
-//   Rumour.find({}, 'id', function (err, tweet_ids) {
-//     greatest_id = '';
+function findGreatestID(callback) {
+  Rumour.find({}, 'id', function (err, tweet_ids) {
+    greatest_id = '';
   
-//     if (tweet_ids.length > 0) {
-//       greatest_id = tweet_ids[0].id;
+    if (tweet_ids.length > 0) {
+      greatest_id = tweet_ids[0].id;
 
-//       for (i = 1; i < tweet_ids.length; i++) {
-//         if (tweet_ids[i].id.localeCompare(greatest_id) == 1){
-//           greatest_id = tweet_ids[i].id;
-//         }
-//       }
-//     }
-//     console.log(greatest_id);
-//     callback(greatest_id);
-//   });
-// }
+      for (i = 1; i < tweet_ids.length; i++) {
+        if (tweet_ids[i].id.localeCompare(greatest_id) == 1){
+          greatest_id = tweet_ids[i].id;
+        }
+      }
+    }
+    console.log(greatest_id);
+    callback(greatest_id);
+  });
+}
 
 // Find rumour hits from messages returned by the Search API.
 
-// function analyseRumourHits(tweets) {
-//   file_string = "";
+function analyseRumourHits(tweets) {
+  file_string = "";
 
-//   for (i = 0; i < tweets.statuses.length; i++) {
-//     tweet = tweets.statuses[i];
-//     tweet_text = JSON.stringify(tweet.text);
+  for (i = 0; i < tweets.statuses.length; i++) {
+    tweet = tweets.statuses[i];
+    tweet_text = JSON.stringify(tweet.text);
 
-//     if ((tweet_text.search(/kylie jenner/i)) && (tweet_text.search(/pregnant/i))){
-//       checkDBandAdd(tweet);
-//     }
-//   }
-// }
+    if ((tweet_text.search(/kylie jenner/i)) && (tweet_text.search(/pregnant/i))){
+      checkDBandAdd(tweet);
+    }
+  }
+}
 
-// // Check DB for tweet received to avoid storing duplicates.
+// Check DB for tweet received to avoid storing duplicates.
 
-// function checkDBandAdd(tweet) {
-//   Rumour.findOne({'id': tweet.id}, function (err, duplicate) {
-//     if (err) return handleError(err);
+function checkDBandAdd(tweet) {
+  Rumour.findOne({'id': tweet.id}, function (err, duplicate) {
+    if (err) return handleError(err);
     
-//     if (duplicate == null) {
-//       db_rumour = parseTweet(tweet);
-//       addToDB(db_rumour);
-//     } else {
-//       console.log('duplicate tweet received');
-//     }
-//   });
-// }
+    if (duplicate == null) {
+      db_rumour = parseTweet(tweet);
+      addToDB(db_rumour);
+    } else {
+      console.log('duplicate tweet received');
+    }
+  });
+}
 
 // Parse relevant data including impact features from Tweet.
 
-// function parseTweet(tweet) {
-//   var rumour = new Rumour();
+function parseTweet(tweet) {
+  var rumour = new Rumour();
 
-//   rumour.id = JSON.stringify(tweet.id);
-//   rumour.text = JSON.stringify(tweet.text);
+  rumour.id = JSON.stringify(tweet.id);
+  rumour.text = JSON.stringify(tweet.text);
 
-//   // Impact / Engagement
-//   rumour.impact_score = calculateImpact(tweet.favorite_count, tweet.retweet_count, tweet.user.followers_count);
-//   rumour.favorite_count = JSON.stringify(tweet.favorite_count);
-//   rumour.retweet_count = JSON.stringify(tweet.retweet_count);
+  // Impact / Engagement
+  rumour.impact_score = calculateImpact(tweet.favorite_count, tweet.retweet_count, tweet.user.followers_count);
+  rumour.favorite_count = JSON.stringify(tweet.favorite_count);
+  rumour.retweet_count = JSON.stringify(tweet.retweet_count);
 
-//   // Tweet features for analysis
-//   rumour.tweet_location = tweet.place ? JSON.stringify(tweet.place.full_name) : tweet.place;
-//   rumour.tweet_createdAt = JSON.stringify(tweet.created_at);
-//   rumour.entities_hashtags = JSON.stringify(tweet.entities.hashtags) == '[]' ? false : true;
-//   rumour.entities_urls = JSON.stringify(tweet.entities.urls) == '[]' ? false : true;
-//   rumour.entities_user_mentions = JSON.stringify(tweet.entities.user_mentions) == '[]' ? false : true;
-//   rumour.entities_media = tweet.entities.media ? true : false;
-//   rumour.word_retweet = tweet_text.search(/retweet/i) > -1 ? true : false;
-//   rumour.all_caps = tweet_text.localeCompare(tweet_text.toUpperCase()) == 0 ? true : false;
-//   rumour.question_mark = tweet_text.indexOf('?') > -1 ? true : false;
-//   rumour.explanation_mark = tweet_text.indexOf('!') > -1 ? true : false;
-//   rumour.quote = tweet.text.replace(/[^\""]/g, "").length == 2 ? true : false;
-//   rumour.smiling_emoticon = tweet_text.indexOf(':)') > -1 ? true : false;
-//   rumour.default_profile = JSON.stringify(tweet.user.default_profile);
-//   rumour.default_profile_image = JSON.stringify(tweet.user.default_profile_image);
-//   rumour.user_createdAt = JSON.stringify(tweet.user.created_at);
-//   rumour.followers_count = JSON.stringify(tweet.user.followers_count);
-//   rumour.friends_count = JSON.stringify(tweet.user.friends_count);
-//   rumour.statuses_count = JSON.stringify(tweet.user.statuses_count);
-//   rumour.account_location = JSON.stringify(tweet.user.location);
-//   rumour.account_verified = JSON.stringify(tweet.user.verified);
+  // Tweet features for analysis
+  rumour.tweet_location = tweet.place ? JSON.stringify(tweet.place.full_name) : tweet.place;
+  rumour.tweet_createdAt = JSON.stringify(tweet.created_at);
+  rumour.entities_hashtags = JSON.stringify(tweet.entities.hashtags) == '[]' ? false : true;
+  rumour.entities_urls = JSON.stringify(tweet.entities.urls) == '[]' ? false : true;
+  rumour.entities_user_mentions = JSON.stringify(tweet.entities.user_mentions) == '[]' ? false : true;
+  rumour.entities_media = tweet.entities.media ? true : false;
+  rumour.word_retweet = tweet_text.search(/retweet/i) > -1 ? true : false;
+  rumour.all_caps = tweet_text.localeCompare(tweet_text.toUpperCase()) == 0 ? true : false;
+  rumour.question_mark = tweet_text.indexOf('?') > -1 ? true : false;
+  rumour.explanation_mark = tweet_text.indexOf('!') > -1 ? true : false;
+  rumour.quote = tweet.text.replace(/[^\""]/g, "").length == 2 ? true : false;
+  rumour.smiling_emoticon = tweet_text.indexOf(':)') > -1 ? true : false;
+  rumour.default_profile = JSON.stringify(tweet.user.default_profile);
+  rumour.default_profile_image = JSON.stringify(tweet.user.default_profile_image);
+  rumour.user_createdAt = JSON.stringify(tweet.user.created_at);
+  rumour.followers_count = JSON.stringify(tweet.user.followers_count);
+  rumour.friends_count = JSON.stringify(tweet.user.friends_count);
+  rumour.statuses_count = JSON.stringify(tweet.user.statuses_count);
+  rumour.account_location = JSON.stringify(tweet.user.location);
+  rumour.account_verified = JSON.stringify(tweet.user.verified);
 
-//   return (rumour);
-// }
+  return (rumour);
+}
 
-// Calculate impact score by user engagement measures.
+Calculate impact score by user engagement measures.
 
-// function calculateImpact(favourites, retweets, followers) {
-//   engagements = ((retweets + favourites) / followers) * 100;
-//   return (engagements);
-// }
+function calculateImpact(favourites, retweets, followers) {
+  engagements = ((retweets + favourites) / followers) * 100;
+  return (engagements);
+}
 
-// // Add rumour to Database.
+// Add rumour to Database.
 
-// function addToDB(rumour) {
-//   rumour.save(function(err, rumour) {
-//     if (err) return console.error(err);
-//     console.log('Tweet added to DB.')
-//   });
-// }
+function addToDB(rumour) {
+  rumour.save(function(err, rumour) {
+    if (err) return console.error(err);
+    console.log('Tweet added to DB.')
+  });
+}
 
 // ------------------------------------------- STATS functions ----------------------------------------------------------//
 
